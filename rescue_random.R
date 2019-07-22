d90b6c03000840b1b5cd8cc0e75f364b65a98cb2
patricksavill <pjsavill@gmail.com>
2019-07-22 12:06:15 +1200

    Simulation with Classes working, needs control refinement

--- rescue_random.R
+++ rescue_random.R
@@ -1,4 +1,4 @@
-rescue_followalong <- R6Class("rescue_followalong", public = list(
+rescue_random <- R6Class("rescue_random", public = list(
   x = 0,
   y = 0,
   speed = 0,
@@ -12,7 +12,7 @@ rescue_followalong <- R6Class("rescue_followalong", public = list(
   run = function(position) {
     # Position passed in is the measured position of the adrift ship
     r_to_target <- sqrt((position[1] - self$x)^2 + (position[2] - self$y)^2)
-    th_to_target <- 180/pi* atan2(position[2] - self$y, position[1] - self$x)
+    th_to_target <- 180/pi* atan2(position[2] - self$y, position[1] - self$x) + rnorm(1) * 180/pi
     
     self$speed <- min(r_to_target, 10)
     self$direction <- th_to_target
--- run_simulation.R
+++ run_simulation.R
@@ -1,40 +1,56 @@
 rm(list = ls()) # Clear the workspace of previous runs
-
+library(R6) # Require the library for class definition used
 set.seed(2) # Set random seed for consistent output in initial testing
 
-# Two hardcoded rescue ships, with two variables, for x and y position
-# TODO make into Classes later
-rescuer_a <- c(0, 0, 0, Inf)
-names(rescuer_a) <- c("x", "y", "velocity", "done")
-rescuer_b <- c(0, 1, 0, Inf)
-names(rescuer_b) <- c("x", "y", "velocity", "done")
 
-rescue_ships <- data.frame(rbind(t(rescuer_a), t(rescuer_b)))
-rownames(rescue_ships) <- c("rescuer_a", "rescuer_b")
+# Setup User Defined Rescue Classes ---------------------------------------
+
+# Here the files in the working directory are added as methods to run with
+# They must be an R6 class with "rescue_" as a prefix
+# Using string manipulation and the "eval" function these are initialized to use later
+# Their "id" is stored in a list to eval to an environment we interact with later
+
+rescue_list <- list.files(pattern = 'rescue_*')
+
+index <- 1
+rescue_ship_ids <- c()
+for (item in rescue_list) {
+  print(item)
+  eval(parse(text = paste("source(\"", item, "\")", sep = "")))
+  
+  rescuer_id <- paste("rescue_", index, sep = "")
+  rescuer_class <- paste(gsub('.R', '', item), "$new(100,100)", sep = "")
+  
+  cat(rescuer_id, "<-", rescuer_class)
+  eval(parse(text = paste(rescuer_id, "<-", rescuer_class)))
+  
+  rescue_ship_ids <- c(rescue_ship_ids, rescuer_id)
+  index <- index + 1
+}
 
 
 # Setup Environment -------------------------------------------------------
 
 
-time_to_rescue <- 180 #
+time_to_rescue <- 180 
 computation_time <- 50e-3
 
-A <- matrix(c(1.005,-0.004, 0.04, 1.005), nrow = 2)
+A <- matrix(c(1.003,-0.009, 0.009, 1.003), nrow = 2, byrow=TRUE)
 
 w_mean <- matrix(c(2, 0), nrow = 2)
-Q_target <- matrix(c(4,-0.7,-0.7, 2), nrow = 2)
+Q_target <- matrix(c(4,-0.7,-0.7, 2), nrow = 2, byrow=TRUE)
 
-Q_rescue <- matrix(c(2.2, 0, 0, 2.2),  nrow = 2)
+Q_rescue <- matrix(c(2, 0, 0, 2),  nrow = 2)
 
 # Starting position for the damaged ship
 target_x <- matrix(c(800 + 100 * rnorm(1), 100), nrow = 2)
 
-R <- 36 ^ 2 * matrix(c(1, 0.2, 0.2, 1), nrow = 2)
+R <- 36 ^ 2 * matrix(c(1, 0.1, 0.1, 1), nrow = 2)
 
 y_prev <- target_x + (chol(R) %*% rnorm(2, 1))
 
-# Starting position for the rescue_ships.
-port_x <- matrix(c(100, 100), nrow = 2)
+# Starting position for the rescue_ship_ids.
+port_x <- matrix(c(200, 100), nrow = 2)
 
 
 X11() # Pop up in a window that will update later
@@ -57,15 +73,15 @@ graph_colours <- c('#0066CC', '#990000', '#333300', '#000000')
 
 time_now <- 0
 
-while (time_now < 10) {
+while (time_now < time_to_rescue) {
   time_now <- time_now + 1
   
   # Check it is within bounds
   if (max(target_x[1], target_x[2]) > 1500) {
-    print("Reached the edge of 15000")
+    print("Reached the edge of 1500")
     break
   }
-  if (max(target_x[1], target_x[2]) < 0) {
+  if (min(target_x[1], target_x[2]) < 0) {
     print("Reached the edge of 0")
     break
   }
@@ -79,104 +95,100 @@ while (time_now < 10) {
   #Sys.sleep(0.5)
   
   
-  # --- Now run the rescue_ships for this time step---
+  # --- Now run the rescue_ship_ids for this time step---
   
   
-  for (ship_index in 1:dim(rescue_ships)[1]){
-    # Get all columns in a single row
-    this_ship = rescue_ships[ship_index, ];
+  for (ship_index in 1:length(rescue_ship_ids)){
+    # Get the environment of each class assigned to a variable
+    this_ship <- eval(parse(text = rescue_ship_ids[ship_index]))
+    
     
     # Assume boat will only be completed if it has
-    if (isTRUE(this_ship["done"])){
+    if (!is.infinite(this_ship$done)){
       print("Rescuer has finished")
       next
     }
     
-    # boat_x = this_ship.x;
-    
     # Measurement noise occurs if the rescuer is moving, determine this
-    
-    if (this_ship["velocity"] >0){
-      R <- 36 ^ 2 * matrix(c(1, 0.2, 0.2, 1), nrow = 2)
+    if (this_ship$speed >0){
+      R <- 36 ^ 2 * matrix(c(1, 0.1, 0.1, 1), nrow = 2)
     }
     else{
-      R <- 2 ^ 2 * matrix(c(1, 0.2, 0.2, 1), nrow = 2)
+      R <- 2 ^ 2 * matrix(c(1, 0.1, 0.1, 1), nrow = 2)
     }
-    
-    # Noisy target observation
-    y <- target_x + chol(R) %*% rnorm(2,1);
-    
-    
     # Ternary of the same if statement
     #R <- ifelse(this_ship["velocity" > 0], 40^2, 2^2 ) * matrix(c(1,0.1,0.1,1), nrow=2)
+    
+    
+    # Noisy target observation relative to the speed of rescuer
+    y <- target_x + chol(R) %*% rnorm(2,1);
     
     # Randomly assign a very wrong measurement
     if (runif(1, 0,1) < 0.1) {
       y <-  target_x + 200^2 * rnorm(2,1);
     }
     
-    # Run the rescue_ships
-    # TODO add in a penalty of delay for overthought rescue_ships    
+    # Run the rescue_ship_ids
     start_time = proc.time()[3]
     
     # Get the velocity and heading from each boat
-    # Todo implementation
-    #heading <- run(y_prev, y)
-    # v <- heading[1], th <- heading[2]
-    
-    v <- 100
-    #th <- runif(1, 0, pi)
-    th <- 90
-    
+    if (this_ship$delayed){
+      this_ship$delayed <- FALSE
+      v <- 0
+      heading <- 0
+    }
+    else{
+      this_ship$run(y)
+      v <- this_ship$speed
+      heading <- this_ship$direction
+    }
+
     stop_time = proc.time()[3]
     
     if (stop_time - start_time > computation_time){
-      print("Rescuer has overheated, having to stop it")
-      # Todo implemented delay of computation
+      print("Rescuer has overheated, having to pause it")
+      this_ship$delayed <- TRUE
     }
     
     # Update the position of this rescue ship
-    
-    this_ship_pos <- this_ship[c(1,2)]
+    this_ship_pos <- matrix(c(this_ship$x, this_ship$y), nrow=2)
     
     # Action of the current, a steady drift
-    this_ship_pos <- as.matrix(this_ship_pos) %*% A
+    this_ship_pos <- A %*% this_ship_pos
     
     # Action of the wind, randomly varying
-    this_ship_pos <- this_ship_pos + t(w_mean + chol(Q_rescue) %*% rnorm(2,1));
+    this_ship_pos <- this_ship_pos + w_mean + chol(Q_rescue) %*% rnorm(2,1)
     
     # Action of the captain
-    this_ship_pos[1] = this_ship_pos[1] + v*cos(pi/180 * th);
-    this_ship_pos[2] = this_ship_pos[2] + v*sin(pi/180 * th);
+    this_ship_pos[1] <- this_ship_pos[1] + v*cos(pi/180 * heading);
+    this_ship_pos[2] <- this_ship_pos[2] + v*sin(pi/180 * heading);
     
-    this_ship[c(1,2)] <- this_ship_pos
-    rescue_ships[ship_index, ] <- this_ship
-    
-    #points(this_ship['x'], this_ship['y'])
+    this_ship$x <- this_ship_pos[1]
+    this_ship$y <- this_ship_pos[2]
     
   }
   
-  # --- Now draw the rescue_ships for this time step---
+  # --- Now draw the rescue ships for this time step, dependent upon their choices ---
   
-  for (ship_index in 1:dim(rescue_ships)[1]){
-    this_ship = rescue_ships[ship_index, ]
+  for (ship_index in 1:length(rescue_ship_ids)){
+    this_ship <- eval(parse(text = rescue_ship_ids[ship_index]))
     
-    if (this_ship["done"] < Inf){
+    if (!is.infinite(this_ship$done)){
       # Check whether the rescuer has suceeded
       next
     }  
     
-    if (sqrt(sum((this_ship[c(1,2)] - target_x)^2)) < 5){
+    print(sqrt(sum((c(this_ship$x, this_ship$y) - target_x)^2)) )
+    if (sqrt(sum((c(this_ship$x, this_ship$y) - target_x)^2)) < 20){
       print(cat('Rescuer is finished at time', t))
-      this_ship["done"] = t;
-      this_ship[c(1,2)] = port_x;
+      this_ship$done <- t;
+      this_ship$x <- port_x[1];
+      this_ship$y <- port_x[2]
     }
-    
-    rescue_ships[ship_index, ] <- this_ship
-    
+
     rescuer_colour = graph_colours[ship_index]
     
-    points(this_ship["x"], this_ship["y"], col=rescuer_colour, pch=ship_index+1)
+    points(this_ship$x, this_ship$y, col=rescuer_colour, pch=ship_index+1)
     
   }
   Sys.sleep(0.1)
